bb8d475ade562e3be1e001cb4f933ac58cee02b4
Jacob Lewis <jake@jakepeterlewis.com>
2016-07-28 10:46:22 +0100

    updates from ages ago, started styling the class page

--- app/js/constants.js
+++ app/js/constants.js
@@ -1,11 +1,7 @@
-
-
 const AppSettings = {
   appTitle: 'Your Class',
   apiUrl: 'http://dev.yourclass.co/web/rest/',
   imageUrl: 'http://dev.yourclass.co/web/',
-  rootUserEmail: 'jakepeterlewis@gmail.com',
-  rootUserId: '83'
 };
 
 export default AppSettings;
--- app/js/controllers/home.js
+++ app/js/controllers/home.js
@@ -1,9 +1,24 @@
-function HomeCtrl($http, $rootScope, $log) {
+function HomeCtrl($http, $rootScope, $log, $uibModal) {
 
   'ngInject';
 
   // ViewModel
   const vm = this;
+
+  vm.aboutModal = function(size) {
+    var modalInstance = $uibModal.open({
+      templateUrl: 'modals/about.html',
+      size: size
+    });
+  };
+
+  $http.get($rootScope.apiUrl + 'classes/list')
+    .then(function successCallback(response) {
+      vm.classesList = response.data.data;
+      $log.info('success' + response);
+    }, function errorCallback(response) {
+      $log.error('error' + response);
+    });
 
 }
 
