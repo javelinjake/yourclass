6c1ba1f0644ce910aec8f8827669d8b12a76346f
Jacob Lewis <jake@jakepeterlewis.com>
2016-09-15 16:50:20 +1000

    Cleaned up run.js, started changing account oages to individual controllers. dashboard and add class are ready, styled tooltip

--- app/js/controllers/user-dashboard.js
+++ app/js/controllers/user-dashboard.js
@@ -5,9 +5,12 @@ function UserDashboardCtrl($http, $rootScope, $log, $filter, Upload, $timeout, $
   const vm = this;
 
   // Get list of listings from user
-  $http.get($rootScope.apiUrl + 'classes/list',  {params: {'_teacherId': $rootScope.userData.id}})
+  $http.get($rootScope.apiUrl + 'classes/list',  {params: {'_teacherId': 200}})
     .then(function successCallback(response) {
       vm.userListings = response.data.data;
+
+      // Set page rendered to true after content loaded
+      //$rootScope.pageRendered = true;
     }, function errorCallback(response) {
     });
 
