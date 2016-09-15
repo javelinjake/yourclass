046dd413b43f445ab937191cbb0d0f604f89b03a
Jacob Lewis <jake@jakepeterlewis.com>
2016-09-15 20:05:26 +1000

    fixed problem with getting the user data

--- app/js/controllers/add-class.js
+++ app/js/controllers/add-class.js
@@ -4,44 +4,60 @@ function UserAddClassCtrl($rootScope, $http, $log) {
   // ViewModel
   const vm = this;
 
-  // Add class basic
-	vm.addClass = function() {
+  // Lets wrap everything in here and wait for the users data to be loaded
+  $rootScope.waitForData.promise.then(function() {
 
-    // Price buttons and custom price
-    vm.customPrice = false;
-    if (vm.priceRadio == 'custom') {
-      vm.price = vm.priceInput;
-    } else {
-      vm.price = vm.priceRadio;
+    // Get categories
+    $http.get($rootScope.apiUrl + 'classes/categories')
+      .then(function successCallback(response) {
+        vm.categories = response.data.data;
+      });
+
+    // Gets the sub categories from selected category
+    vm.classCategory = function(category) {
+      vm.subCategory = category;
     }
 
-    // Size buttons and custom size
-    vm.customSize = false;
-    if (vm.sizeRadio == 'custom') {
-      vm.size = vm.sizeInput;
-    } else {
-      vm.size = vm.sizeRadio;
+    // Add class basic
+    vm.addClass = function() {
+
+      // Price buttons and custom price
+      vm.customPrice = false;
+      if (vm.priceRadio == 'custom') {
+        vm.price = vm.priceInput;
+      } else {
+        vm.price = vm.priceRadio;
+      }
+
+      // Size buttons and custom size
+      vm.customSize = false;
+      if (vm.sizeRadio == 'custom') {
+        vm.size = vm.sizeInput;
+      } else {
+        vm.size = vm.sizeRadio;
+      }
+
+      var classData = {
+        'title': vm.title,
+        'teacherId': $rootScope.userData.id,
+        'categoryId': vm.categoryId,
+        'subcategoryId': vm.subcategoryId,
+        'price': vm.price,
+        'size': vm.size,
+        'venueType': 'private' // Default for now
+      };
+
+      $http.post($rootScope.apiUrl + 'classes/create', classData).success((data) => {
+        // Redirect to edit class
+
+        // Setting coming from add class to true
+        $rootScope.fromAddClass = true;
+      }).error((err, status) => {
+
+      });
     }
 
-		var classData = {
-			'title': vm.title,
-			'teacherId': 200, //TODO - get from $rootScope.userData.id
-      'categoryId': vm.categoryId,
-      'subcategoryId': vm.subcategoryId,
-			'price': vm.price,
-			'size': vm.size,
-			'venueType': 'private' // Default for now
-		};
-
-		$http.post($rootScope.apiUrl + 'classes/create', classData).success((data) => {
-      vm.showAddClass = false;
-      vm.showAddClassOptional = true;
-
-      vm.currentClassTitle = vm.title;
-		}).error((err, status) => {
-
-		});
-	}
+  });
 
 }
 
--- app/js/controllers/user-dashboard.js
+++ app/js/controllers/user-dashboard.js
@@ -1,18 +1,19 @@
-function UserDashboardCtrl($http, $rootScope, $log, $filter, Upload, $timeout, $state) {
+function UserDashboardCtrl($http, $rootScope, $log) {
   'ngInject';
 
   // ViewModel
   const vm = this;
 
-  // Get list of listings from user
-  $http.get($rootScope.apiUrl + 'classes/list',  {params: {'_teacherId': 200}})
-    .then(function successCallback(response) {
-      vm.userListings = response.data.data;
+  // Lets wrap everything in here and wait for the users data to be loaded
+  $rootScope.waitForData.promise.then(function() {
 
-      // Set page rendered to true after content loaded
-      //$rootScope.pageRendered = true;
-    }, function errorCallback(response) {
-    });
+    // Get list of listings from user
+    $http.get($rootScope.apiUrl + 'classes/list',  {params: {'_teacherId': $rootScope.userData.id}})
+      .then(function successCallback(response) {
+        vm.userListings = response.data.data;
+      });
+
+  });
 
 }
 
