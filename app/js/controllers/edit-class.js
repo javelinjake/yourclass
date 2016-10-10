a619909be03b87e27582f417963f5df7d268231b
Jacob Lewis <jake@jakepeterlewis.com>
2016-10-10 13:40:10 +1100

    edit class details done

--- app/js/controllers/edit-class.js
+++ app/js/controllers/edit-class.js
@@ -1,4 +1,4 @@
-function UserEditClassCtrl($rootScope, $http, $log, getEditClassAlias, $state, $q) {
+function UserEditClassCtrl($rootScope, $http, $log, getEditClassAlias, $state, $q, Upload, $timeout) {
   'ngInject';
 
   // ViewModel
@@ -21,6 +21,11 @@ function UserEditClassCtrl($rootScope, $http, $log, getEditClassAlias, $state, $
     // Lets also wait for the class data to be loaded
     vm.waitForClassData.promise.then(function() {
 
+      vm.tabIndex = 0;
+      if ($rootScope.fromAddClass) {
+        vm.tabIndex = 1;
+      }
+
       $log.info(vm.classData.category.id);
 
       //vm.classData.category.id.selected = vm.classData.category.id[0];
@@ -36,33 +41,48 @@ function UserEditClassCtrl($rootScope, $http, $log, getEditClassAlias, $state, $
         vm.subCategory = category;
       }
 
-      // Add class basic
-      vm.editClass = function() {
+      // If custom price has been chosen
+      if (vm.classData.price == '0.00' || vm.classData.price == '10.00' || vm.classData.price == '20.00' || vm.classData.price == '30.00') {
+        vm.customPrice = false;
+      } else {
+        vm.customPrice = true;
+        vm.priceInput = Number(vm.classData.price);
+      }
+      // If custom size has been chosen
+      if (vm.classData.size == '1' || vm.classData.size == '2' || vm.classData.size == '5' || vm.classData.size == '10' || vm.classData.size == '20') {
+        vm.customSize = false;
+      } else {
+        vm.customSize = true;
+        vm.sizeInput = Number(vm.classData.size);
+      }
 
-        console.log(vm.addClassForm.title.$pristine);
+      // Add class basic
+      vm.editClassBasics = function() {
+
+        // console.log(vm.addClassForm.title.$pristine);
 
         // Price buttons and custom price
-        vm.customPrice = false;
-        if (vm.priceRadio == 'custom') {
-          vm.price = vm.priceInput;
-        } else {
-          vm.price = vm.priceRadio;
+        //vm.customPrice = false;
+        if (vm.classData.price == 'custom') {
+          vm.classData.price = vm.priceInput;
         }
 
         // Size buttons and custom size
-        vm.customSize = false;
-        if (vm.sizeRadio == 'custom') {
-          vm.size = vm.sizeInput;
-        } else {
-          vm.size = vm.sizeRadio;
+        //vm.customSize = false;
+        if (vm.classData.size == 'custom') {
+          vm.classData.size = vm.sizeInput;
         }
+
+        // if (!vm.classData.subcategory.id) {
+        //   vm.classData.subcategory.id = "";
+        // }
 
         var classData = {
           '_auth_key': $rootScope.authToken,
           '_id': vm.classData.id,
           'title': vm.classData.title,
-          'categoryId': vm.classData.category.id,
-          'subcategoryId': vm.classData.subcategory.id,
+          //'categoryId': vm.classData.category.id,
+          //'subcategoryId': vm.classData.subcategory.id,
           'price': vm.classData.price,
           'size': vm.classData.size
           //'venueType': 'private' // Default for now
@@ -70,7 +90,7 @@ function UserEditClassCtrl($rootScope, $http, $log, getEditClassAlias, $state, $
 
         $http.post($rootScope.apiUrl + 'classes/update', classData).success((data) => {
           $log.info(data);
-          $log.info('Class updated');
+          $log.info('Class updated the basics');
 
           // convert title to alias TODO - Check with Slava and put in filters?
           function slugify(text)
@@ -85,15 +105,72 @@ function UserEditClassCtrl($rootScope, $http, $log, getEditClassAlias, $state, $
 
           // Check to see if title was changed
           if (!vm.addClassForm.title.$pristine) {
+            // Set to false as you will be redirected to details tab
+            $rootScope.fromAddClass = false;
             // Reload with new URL
             $state.go('User-Edit-Class', {classEditName: slugify(vm.classData.title)});
           } else {
-            // Just reload the page
-            $state.reload();
+            // Just reload the page TODO - Any need to reload?
+            // $state.reload();
+
+            //vm.tabIndex = 0;
           }
 
         }).error((err, status) => {
 
+        });
+      }
+
+      // Add class details
+      vm.editClassDetails = function() {
+
+        var classData = {
+          '_auth_key': $rootScope.authToken,
+          '_id': vm.classData.id,
+          'brief': vm.classData.brief,
+          'description': vm.classData.description
+        };
+
+        $http.post($rootScope.apiUrl + 'classes/update', classData).success((data) => {
+          $log.info(data);
+          $log.info('Class updated the details');
+
+          // Just reload the page
+          //$state.reload();
+
+          //vm.tabIndex = 1;
+
+        }).error((err, status) => {
+
+        });
+      }
+
+      vm.uploadFiles = function(files, errFiles) {
+        vm.files = files;
+        vm.errFiles = errFiles;
+        angular.forEach(files, function(file) {
+          file.upload = Upload.upload({
+            url: $rootScope.apiUrl + 'classes/upload',
+            data: {
+              'id': vm.classData.id,
+              '_auth_key': $rootScope.authToken,
+              'file': file
+            }
+          });
+
+          file.upload.then(function(response) {
+            $timeout(function() {
+              file.result = response.data;
+              $log.info("Image response: ", response.data);
+              // $state.reload();
+            });
+          }, function(response) {
+            if (response.status > 0)
+              vm.errorMsg = response.status + ': ' + response.data;
+          }, function(evt) {
+            file.progress = Math.min(100, parseInt(100.0 *
+              evt.loaded / evt.total));
+          });
         });
       }
 
