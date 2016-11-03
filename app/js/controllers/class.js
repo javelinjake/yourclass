29cfda90f0510f123a56476a0ed70737fc4cb0b2
maryna.pidlisna <maryna.pidlisna@ideus.biz>
2016-11-04 00:33:50 +0200

    Booking block. Structure.

--- app/js/controllers/class.js
+++ app/js/controllers/class.js
@@ -1,6 +1,6 @@
 
 
-function ClassCtrl($http, $rootScope, $scope, getClassAlias) {
+function ClassCtrl($http, $rootScope, $scope, $log, getClassAlias) {
 	'ngInject';
 
 	// ViewModel
@@ -9,7 +9,50 @@ function ClassCtrl($http, $rootScope, $scope, getClassAlias) {
 	// Get Classes
 	$http.get($rootScope.apiUrl + 'classes/one', {params: {_alias: getClassAlias}}).success((data) => {
 		vm.class = data.data;
+
+    // Update booking price
+    vm.booking.price = vm.class.price;
 	}).error((err, status) => {});
+
+  vm.booking = {
+    spots: 6,
+    friends: {
+      count: 0,
+      less: function() {
+        var count = vm.booking.friends.count;
+        vm.booking.friends.count = count > 0 ? --count : 0;
+      },
+      more: function() {
+        var count = vm.booking.friends.count,
+            limit = vm.booking.friends.limit;
+        vm.booking.friends.count = count < limit ? ++count : limit;
+      },
+      limit: 5
+    },
+    list: new Array(10),
+    price: 0,
+    select: function(e) {
+      console.log(arguments);
+    },
+    selected: [],
+    view: {
+      expanded: true,
+      toggle: function() {
+        vm.booking.view.expanded = !vm.booking.view.expanded;
+      },
+      limit: 5
+    },
+    submit: function() {
+      console.log('submit');
+
+      if (vm.booking.selected.length == 0) {
+        vm.booking.error = true;
+      } else {
+        vm.booking.error = false;
+      }
+    },
+    error: false
+  };
 
 }
 
