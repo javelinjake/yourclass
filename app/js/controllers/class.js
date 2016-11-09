1956e0308d825f47eb67e8ba80f19cd4d2968179
maryna.pidlisna <maryna.pidlisna@ideus.biz>
2016-11-09 21:39:18 +0200

    Booking. Edits.

--- app/js/controllers/class.js
+++ app/js/controllers/class.js
@@ -14,8 +14,74 @@ function ClassCtrl($http, $rootScope, $scope, $log, getClassAlias) {
     vm.booking.price = vm.class.price;
 	}).error((err, status) => {});
 
+
+  /**
+   * BOOKING
+   * -------
+   * One item has: date, time, size and number of bookings
+   *
+   */
+  var bookingData = [
+    {
+      id: 1,
+      date: {
+        start: new Date('2016-08-15' + ' ' + '02:00:00'),
+        end: new Date('2016-08-15' + ' ' + '03:00:00')
+      },
+      size: 10,
+      left: 8
+    },
+    {
+      id: 2,
+      date: {
+        start: new Date('2016-08-15' + ' ' + '02:00:00'),
+        end: new Date('2016-08-15' + ' ' + '03:00:00')
+      },
+      size: 10,
+      left: 8
+    },
+    {
+      id: 3,
+      date: {
+        start: new Date('2016-08-15' + ' ' + '02:00:00'),
+        end: new Date('2016-08-15' + ' ' + '03:00:00')
+      },
+      size: 10,
+      left: 8
+    },
+    {
+      id: 4,
+      date: {
+        start: new Date('2016-08-15' + ' ' + '02:00:00'),
+        end: new Date('2016-08-15' + ' ' + '03:00:00')
+      },
+      size: 10,
+      left: 8
+    },
+    {
+      id: 5,
+      date: {
+        start: new Date('2016-08-15' + ' ' + '02:00:00'),
+        end: new Date('2016-08-15' + ' ' + '03:00:00')
+      },
+      size: 10,
+      left: 8
+    },
+    {
+      id: 6,
+      date: {
+        start: new Date('2016-08-15' + ' ' + '02:00:00'),
+        end: new Date('2016-08-15' + ' ' + '03:00:00')
+      },
+      size: 10,
+      left: 8
+    }
+  ];
+  $log.info(bookingData);
+
   vm.booking = {
     spots: 6,
+
     friends: {
       count: 0,
       less: function() {
@@ -29,30 +95,42 @@ function ClassCtrl($http, $rootScope, $scope, $log, getClassAlias) {
       },
       limit: 5
     },
-    list: new Array(10),
+
+    list: bookingData, // TEMP
+
     price: 0,
+
     select: function(e) {
       console.log(arguments);
     },
-    selected: [],
+    selected: null, // an obj with properties
+
     view: {
-      expanded: true,
+      expanded: false,
       toggle: function() {
         vm.booking.view.expanded = !vm.booking.view.expanded;
       },
       limit: 5
     },
+
     submit: function() {
       console.log('submit');
 
-      if (vm.booking.selected.length == 0) {
+      if (vm.booking.selected.length === 0) {
         vm.booking.error = true;
       } else {
         vm.booking.error = false;
       }
     },
+
     error: false
   };
+
+  /* Watch Selected date and update Booking data */
+  $scope.$watch('class.booking.selected', function(next, prev) {
+    vm.booking.spots = next.left || 0;
+    vm.booking.friends.limit = next.left - 1 || 0;
+  });
 
 }
 
