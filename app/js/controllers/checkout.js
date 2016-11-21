83d61216984f25da724bf895179c165cc9380c62
maryna.pidlisna <maryna.pidlisna@ideus.biz>
2016-11-21 21:54:45 +0200

    Checkout. Payment.

--- app/js/controllers/checkout.js
+++ app/js/controllers/checkout.js
@@ -18,7 +18,25 @@ function CheckoutCtrl($http, $rootScope, $scope, $log, $cookies, $state, $stateP
   };
 
   // vm.details = {};
-  // vm.payment = {};
+  vm.payment = {
+    billing: {
+      firstName: '',
+      lastName: '',
+      address_1: '',
+      address_2: '',
+      suburb: '',
+      state: '',
+      postcode: ''
+    },
+    card: {
+      number: '',
+      start: '',
+      expiry: '',
+      cvc: '',
+      save: false,
+      agreement: false
+    }
+  };
   // vm.confirmation = {};
 
 
@@ -36,9 +54,9 @@ function CheckoutCtrl($http, $rootScope, $scope, $log, $cookies, $state, $stateP
   // Create an array of students
   for (var i = 0; i < vm.students.length; i++) {
     vm.students[i] = {
-      name: null,
-      email: null,
-      number: null
+      name: '',
+      email: '',
+      number: ''
     };
   }
 
@@ -47,10 +65,10 @@ function CheckoutCtrl($http, $rootScope, $scope, $log, $cookies, $state, $stateP
     if (!next) { /*$state.go('Home');*/ return false; }
 
     vm.user = next;
-    vm.students[0].id     = next.id || null;
-    vm.students[0].name   = (next.profile.firstName || null) + ' ' + (next.profile.lastName || null);
-    vm.students[0].email  = next.email || null;
-    vm.students[0].number = next.profile.number || null;
+    vm.students[0].id     = next.id || '';
+    vm.students[0].name   = (next.profile.firstName || '') + ' ' + (next.profile.lastName || '');
+    vm.students[0].email  = next.email || '';
+    vm.students[0].number = next.profile.number || '';
   });
 }
 
